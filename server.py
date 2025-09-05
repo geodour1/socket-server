f1ad58c81fc950c0ce7dbca1d684b1b8b46456bc
geodour1 <giorgos.dour@gmail.com>
2025-09-06 01:43:42 +0300

    logging config

--- server.py
+++ server.py
@@ -2,7 +2,13 @@ import asyncio
 import websockets
 from websockets import ClientConnection
 import logging
+import sys
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
 logger = logging.getLogger("websocket-server")
 
 class Server:
